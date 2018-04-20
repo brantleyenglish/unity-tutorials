95afa797bd657ff8602b3fa6ad22b4b837971dcb
Brantley English <brantley@sodiumhalogen.com>
2018-04-20 07:47:41 -0500

    🚀  RELSEASE: Starts Tutorial 2

---
+++ Terminal/Assets/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu ();
+	}
+
+	void ShowMainMenu () {
+		var greeting = "Hello Hacker";
+		Terminal.ClearScreen();
+		Terminal.WriteLine(greeting);
+		Terminal.WriteLine("What difficulty would you like?");
+		Terminal.WriteLine("Press 1 for easy");
+		Terminal.WriteLine("Press 2 for medium");
+		Terminal.WriteLine("Press 3 for hard");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
